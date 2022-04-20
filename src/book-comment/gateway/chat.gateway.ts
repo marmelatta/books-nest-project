e69ce28f13a47c4d60093151bc6bc13712742706
marmelatta <testandroidapp@yandex.ru>
2022-04-20 23:46:48 +0500

    rooms

--- src/book-comment/gateway/chat.gateway.ts
+++ src/book-comment/gateway/chat.gateway.ts
@@ -14,8 +14,19 @@ export class ChatGateway implements OnGatewayInit {
   }
   
   @SubscribeMessage('chatToServer')
-  handleMessage(client: Socket, message: { sender: string, message: string }) {
-    this.wss.emit('chatToClient', message);
+  handleMessage(client: Socket, message: { sender: string, room: string, message: string }) {
+    this.wss.to(message.room).emit('chatToClient', message);
   }
 
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(client: Socket, room: string) {
+    client.join(room);
+    client.emit('joinedRoom', room);
+  }
+
+  @SubscribeMessage('leaveRoom')
+  handleLeaveRoom(client: Socket, room: string) {
+    client.leave(room);
+    client.emit('leftRoom', room);
+  }
 }
